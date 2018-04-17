4ae82d63313c887f837340e721f30e2093b43bf5
vickyfegan <37214260+vickyfegan@users.noreply.github.com>
2018-04-17 21:59:37 +0100

    Youtube
    Videos of club

---
+++ app/src/main/java/com/example/declan/clubmoy/PlayerConfig.java
@@ -0,0 +1,12 @@
+package com.example.declan.clubmoy;
+
+public class PlayerConfig {
+
+    PlayerConfig()
+    {
+
+    }
+
+    public static final String API_KEY = "AIzaSyC5qhcnblqrW7QHY4zUkDIWvbkcz_OFpjQ";
+}
+//https://www.youtube.com/watch?v=3LiubyYpEUk
