1f972d18d3fb4c5e69d5b02220232741e80c17a3
vickyfegan <37214260+vickyfegan@users.noreply.github.com>
2018-04-21 19:09:12 +0100

    homepage
    home page complete

--- app/src/main/java/com/example/declan/clubmoy/Homepage.java
+++ app/src/main/java/com/example/declan/clubmoy/Homepage.java
@@ -1,5 +1,6 @@
 package com.example.declan.clubmoy;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,14 +8,75 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
+import com.example.declan.clubmoy.Booking.BookingFacilities;
+import com.example.declan.clubmoy.Booking.Calendar;
+import com.example.declan.clubmoy.Payments.Paypal;
+
 public class Homepage extends AppCompatActivity {
 
     ViewFlipper v_flipper;
+    ImageView bookingImage, moneyImage, calendarImage, locationImage, football, logoutImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        bookingImage = (ImageView)findViewById(R.id.bookingImage);
+        moneyImage = (ImageView)findViewById(R.id.moneyImage);
+        calendarImage = (ImageView)findViewById(R.id.calendarImage);
+        locationImage = (ImageView)findViewById(R.id.locationImage);
+        football = (ImageView)findViewById(R.id.football);
+        logoutImage = (ImageView)findViewById(R.id.logoutImage);
+
+        bookingImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, BookingFacilities.class);
+                startActivity(i);
+            }
+        });
+
+        moneyImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, Paypal.class);
+                startActivity(i);
+            }
+        });
+
+        calendarImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, Calendar.class);
+                startActivity(i);
+            }
+        });
+
+        locationImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+       /* football.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, Training.class);
+                startActivity(i);
+            }
+        });*/
+
+        /*logoutImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Homepage.this, Logout.class);
+                startActivity(i);
+            }
+        });*/
+
 
         int images[] = {R.drawable.carrickmore, R.drawable.parhall, R.drawable.crokepark, R.drawable.gaeltacht, R.drawable.semifinal, R.drawable.tyrone};
 
@@ -39,5 +101,7 @@ public class Homepage extends AppCompatActivity {
         v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
 
     }
+
+
 
 }
