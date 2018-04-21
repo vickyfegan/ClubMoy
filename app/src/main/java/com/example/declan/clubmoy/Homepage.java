12d2d171eab41f702b8c486ff21cf102dd666dc6
vickyfegan <37214260+vickyfegan@users.noreply.github.com>
2018-04-21 18:20:10 +0100

    Image
    Homepage image flipper

--- app/src/main/java/com/example/declan/clubmoy/Homepage.java
+++ app/src/main/java/com/example/declan/clubmoy/Homepage.java
@@ -1,13 +1,43 @@
 package com.example.declan.clubmoy;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Homepage extends AppCompatActivity {
+
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        int images[] = {R.drawable.carrickmore, R.drawable.parhall, R.drawable.crokepark, R.drawable.gaeltacht, R.drawable.semifinal, R.drawable.tyrone};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for(int image: images)
+        {
+            flipperImages(image);
+        }
     }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000); //4Seconds
+        v_flipper.setAutoStart(true);
+
+        //animation
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
+    }
+
 }
